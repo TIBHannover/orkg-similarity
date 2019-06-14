02929878efe0b9fcaf5a8e0c197fcc8c588019a8
YaserJaradeh <Yaser.Jaradeh@tib.eu>
2019-06-14 15:45:18 +0200

    adapt to multiple predicates with multiple values for shared contributions

--- compare.py
+++ compare.py
@@ -3,7 +3,8 @@ from py2neo import Graph
 import numpy as np
 import pandas as pd
 import itertools
-import statistics
+
+
 
 # import requests, zipfile, io
 # ft_url = 'https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki-news-300d-1M-subword.bin.zip'
@@ -53,6 +54,10 @@ def get_sub_graph_predicates(resource):
                                                                                             "as object_id")]
 
 
+def get_sub_graph(resource):
+    return [(x['predicate'], x['object'], x['object_id'], x['literal_id']) for x in graph.run("MATCH (n:Resource {resource_id: \""+resource+"\"}) CALL apoc.path.subgraphAll(n, {relationshipFilter:'>', bfs:true}) YIELD relationships UNWIND relationships as rel RETURN startNode(rel).label as subject, startNode(rel).resource_id as subject_id, rel.predicate_id as predicate, endNode(rel).label as object, endNode(rel).resource_id as object_id, endNode(rel).literal_id as literal_id")]
+
+
 def _remove_used_preds(to_compare, found):
     temp = []
     for list in to_compare:
@@ -61,7 +66,6 @@ def _remove_used_preds(to_compare, found):
 
 
 def _get_common_among_k(tuples, found, k):
-    print(f'dic: {found}, k: {k}')
     new_found = found
     if k > len(tuples[0]):
         return new_found
@@ -81,17 +85,46 @@ def _get_common_among_k(tuples, found, k):
     return new_found
 
 
+def remove_redundant_entries(entries):
+    to_remove = []
+    for pred, info in entries.items():
+        for pred_2, info_2 in entries.items():
+            if pred == pred_2:
+                continue
+            if info_2['freq'] > info['freq']:
+                if pred in info_2['similar']:
+                    to_remove.append(pred)
+    return {key: value for key, value in entries.items() if key not in list(set(to_remove))}
+
+
 def get_common_predicates(resources):
     to_compare = [list(set(get_sub_graph_predicates(res))) for res in resources]
-    # to_compare = [['P9', 'P1001', 'P6'], ['P10', 'P1001', 'P7'], ['P1003', 'P1001', 'P6']]
-    # dic = {common: len(resources) for common in set.intersection(*[set(list) for list in to_compare])}
-    # to_compare = _remove_used_preds(to_compare, dic)
     tuples = list(itertools.product(*to_compare))
     dic = _get_common_among_k(tuples, {item: {'freq': 1, 'similar': {item}} for item in set([x for y in to_compare for x in y])}, 2)
-    print(f'final dic: {dic}')
     return dic
+
+
+def compare_resources(resources):
+    out_contributions = resources  # TODO: (OUTPUT) get paperId, Title, and Label + id of contribution
+    common = remove_redundant_entries(get_common_predicates(resources))
+    # TODO: (OUTPUT) add path??
+    out_predicates = [{'id': key, 'label': predicates[key], 'contributionAmount': value['freq'], 'active':True if value['freq'] == len(resources) else False} for key, value in common.items()]
+    graphs = {res: get_sub_graph(res) for res in resources}
+    data = {}
+    for res, content in graphs.items():
+        for tup in content:
+            if tup[0] in common or tup[0] in [i for i in [value['similar'] for value in common.values()]]:
+                if tup[0] not in data:
+                    data[tup[0]] = {}
+                if res not in data[tup[0]]:
+                    data[tup[0]][res] = []
+                data[tup[0]][res].append({'label': tup[1],
+                                          'resourceId': tup[2] if tup[2] is not None else tup[3],
+                                          'type': 'resource' if tup[2] is not None else 'literal'})
+    out_data = {pred: [content[res] if res in content else [{}] for res in resources] for pred, content in data.items()}
+    return out_contributions, out_predicates, out_data
 
 
 if __name__ == '__main__':
     pred_sim_matrix = compute_similarity_among_predicates()
-    x = get_common_predicates(["R1301", "R1311", "R0"])
+    conts, preds, data = compare_resources(["R1301", "R1311", "R0"])
