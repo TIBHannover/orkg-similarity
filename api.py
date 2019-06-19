11da46d831ec74bb475eec8b37423c46b839f19c
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2019-06-19 11:12:45 +0200

    Add CORS headers, and skeleton similarity calls

--- api.py
+++ api.py
@@ -2,9 +2,20 @@ from flask import Flask, jsonify, make_response
 from util import ListConverter
 
 import compare
+import cache
 
 app = Flask(__name__)
 app.url_map.converters['list'] = ListConverter
+
+
+@app.after_request
+def add_headers(response):
+    response.headers.add('Content-Type', 'application/json')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Expose-Headers', 'Content-Type,Content-Length,Authorization,X-Pagination')
+    return response
 
 
 @app.errorhandler(404)
@@ -20,10 +31,30 @@ def compare_resources(contributions: list):
     return jsonify({'contributions': conts, 'properties': preds, 'data': data})
 
 
+@app.route('/internal/init')
+def setup_similarity():
+    store = cache.Cache()
+    store.create_new_cache()
+    store.save_cache()
+    return "done initing baby!!"
+
+
+@app.route('/similar/<contribution_id>')
+def compute_similarity(contribution_id):
+    store = cache.Cache()
+    store.load_cache()
+    similar = store.get_top_similar(contribution_id, 5)
+    return jsonify([{'paperId': item[0]['paperId'],
+                     'contributionId': item[0]['id'],
+                     'contributionLabel': item[0]['contributionLabel'],
+                     'similarityPercentage': item[1]
+                     } for item in [(compare.get_contribution_details(cont), sim) for cont, sim in similar.items()]])
+
+
 @app.route('/')
 def index():
     return "Welcome to the Magic of ORKG"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
