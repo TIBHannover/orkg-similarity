2ca1ace1acb2fe7958cd183f70cc3e9d904d3571
yaser <yaser.jaradeh@gmail.com>
2020-03-27 12:55:59 +0100

    Remove not-needed escapes for elastic

--- similarity/elastic_similarity.py
+++ similarity/elastic_similarity.py
@@ -46,8 +46,8 @@ escape_rules = {'+': r'\+',
                 '&': r'\&',
                 '|': r'\|',
                 '!': r'\!',
-                '(': r'\(',
-                ')': r'\)',
+                # '(': r'\(',  # Removed because ES didn't recognize the escape char
+                # ')': r'\)',  # Removed because ES didn't recognize the escape char
                 '{': r'\{',
                 '}': r'\}',
                 '[': r'\[',
@@ -56,9 +56,9 @@ escape_rules = {'+': r'\+',
                 '~': r'\~',
                 '*': r'\*',
                 '?': r'\?',
-                ':': r'\:',
+                # ':': r'\:',  # Removed because ES didn't recognize the escape char
                 '"': r'\"',
-                '\\': r'\\;',
+                '\\': r'\\\\',
                 '/': r'\/',
                 '>': r' ',
                 '<': r' '}
@@ -103,7 +103,7 @@ def query_index(cont, top_k=5):
 if __name__ == '__main__':
     # create_index()
     # index_document("R61003")
-    similar = query_index("R91001")
+    similar = query_index("R8186")
     # results = sorted([{'paperId': item[0]['paperId'],
     #                   'contributionId': item[0]['id'],
     #                   'contributionLabel': item[0]['contributionLabel'],
